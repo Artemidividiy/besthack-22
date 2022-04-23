9805299c001df89afdfc3b08f802cebc24f452e8
notblank00 <pyromancy00@gmail.com>
2022-04-23 13:59:05 +0300

    initialized spring

---
+++ besthack22/src/main/java/ru/baza134/besthack22/Besthack22Application.java
@@ -0,0 +1,13 @@
+package ru.baza134.besthack22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Besthack22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Besthack22Application.class, args);
+	}
+
+}
---
+++ besthack22/src/test/java/ru/baza134/besthack22/Besthack22ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.baza134.besthack22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Besthack22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
