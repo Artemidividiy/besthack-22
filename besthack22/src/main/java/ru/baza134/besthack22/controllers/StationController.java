4ebf0166e6b068b2b24b03e60f3d114750b1a56e
notblank00 <pyromancy00@gmail.com>
2022-04-24 06:50:57 +0300

    almost there except not enough factories

---
+++ besthack22/src/main/java/ru/baza134/besthack22/controllers/StationController.java
@@ -0,0 +1,75 @@
+package ru.baza134.besthack22.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.baza134.besthack22.factories.CSVStationFactory;
+import ru.baza134.besthack22.factories.JSONStationFactory;
+import ru.baza134.besthack22.factories.StationFactory;
+import ru.baza134.besthack22.fetchers.Fetcher;
+import ru.baza134.besthack22.fetchers.LocalFetcher;
+import ru.baza134.besthack22.fetchers.RemoteFetcher;
+import ru.baza134.besthack22.models.Station;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class StationController {
+  
+  @RequestMapping(value="/", method=RequestMethod.POST)
+  public ResponseEntity<?> fetch(@RequestBody String requestData) {
+    JacksonJsonParser parser = new JacksonJsonParser();
+    Map<String, Object> data = parser.parseMap(requestData);
+    String fetchMode = (String) data.get("fetchMode");
+    String dataSource = (String) data.get("dataSource");
+    String contentType = (String) data.get("contentType");
+    Fetcher fetcher;
+    switch(fetchMode) {
+      case "local":
+        fetcher = new LocalFetcher();
+        break;
+      case "remote":
+        fetcher = new RemoteFetcher();
+        break;
+      default:
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+    String sourceSerialized;
+    if(dataSource == null)
+      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    try {
+      sourceSerialized = fetcher.fetchSerialized(dataSource);
+    } catch (Exception exception) {
+      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+    StationFactory stationFactory;
+    switch(contentType) {
+      case "json":
+        stationFactory = new JSONStationFactory();
+        break;
+      case "csv":
+        stationFactory = new CSVStationFactory();
+        break;
+      default:
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+    try {
+      Map<String, String> mappingRules;
+      for(Map.Entry<String, Object> entry : data.entrySet()) {
+        if(entry.getKey().substring(0, 8) == "station.") {
+          mappingRules.put((String) entry.getValue(), entry.getKey());
+        }
+      }
+      List<Station> stations = stationFactory.createStations();
+    } catch (Exception exception) {
+      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
--- besthack22/src/main/java/ru/baza134/besthack22/factories/JSONStationFactory.java
+++ besthack22/src/main/java/ru/baza134/besthack22/factories/JSONStationFactory.java
@@ -1,17 +1,30 @@
 package ru.baza134.besthack22.factories;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.boot.json.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import ru.baza134.besthack22.models.Station;
 
-// public class JSONStationFactory extends StationFactory {
-//   @Override
-//   public List<Station> createStations(Map<String, String> mappingRules, String payload) {
-//     JsonParser parser = new JacksonJsonParser();
-//     Map<String, Object> data = parser.parseMap(payload);
-//   }
-// }
+public class JSONStationFactory extends StationFactory {
+  @Override
+  public List<Station> createStations(Map<String, String> mappingRules, String payload) throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
+    ArrayNode arrayNode = (ArrayNode) objectMapper.readTree(payload);
+    List<Station> stations = new LinkedList<>();
+    arrayNode.forEach(sourceEntity -> {
+      Station station = new Station();
+      mappingRules.keySet().forEach(key -> {
+        JsonNode valueNode = sourceEntity.findValue(key);
+        if(valueNode != null)
+          getPropertyAttachmentHelper().tryAttachPropertyToStation(station, mappingRules.get(key), valueNode.asText());
+      });
+      stations.add(station);
+    });
+    return stations;
+  }
+}
